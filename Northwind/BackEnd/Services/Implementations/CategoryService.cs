d1356bbdeab34081e7f663d520f3ad3c2636bf4e
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-09-19 19:36:05 -0600

    Category Service Get All Categories

---
+++ Northwind/BackEnd/Services/Implementations/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+
+        public IUnidadDeTrabajo _unidadDeTrabajo;
+
+        public CategoryService(IUnidadDeTrabajo unidadDeTrabajo)
+        {
+                _unidadDeTrabajo = unidadDeTrabajo;
+        }
+
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            IEnumerable<Category> categories;
+            categories = await _unidadDeTrabajo._categoryDAL.GetAll();
+            return categories;
+        }
+    }
+}
---
+++ Northwind/BackEnd/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Entities;
+
+namespace BackEnd.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+
+    }
+}
