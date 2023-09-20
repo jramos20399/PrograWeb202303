09617bc0bc4a1bf307e51a8e41bf0a19e5656f6c
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-09-19 19:14:27 -0600

    GetALL Genereric implemented

---
+++ Northwind/DAL/Implementations/DALGenericoImpl.cs
@@ -0,0 +1,65 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class DALGenericoImpl<TEntity> : IDALGenerico<TEntity> where TEntity : class
+    {
+
+        protected readonly NorthWindContext _Context;
+
+
+        public DALGenericoImpl(NorthWindContext context)
+        {
+            _Context = context;
+        }
+
+
+
+        public bool Add(TEntity entity)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            try
+            {
+                return await _Context.Set<TEntity>().ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
