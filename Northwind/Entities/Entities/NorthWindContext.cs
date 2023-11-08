c38da885e91914e7c7f7d98c2b52611755baa7f5
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-11-07 19:35:26 -0600

    Set Util in northwindContext

--- Northwind/Entities/Entities/NorthWindContext.cs
+++ Northwind/Entities/Entities/NorthWindContext.cs
@@ -50,11 +50,8 @@ namespace Entities.Entities
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Database=NorthWind;Integrated Security=True;Trusted_Connection=True;");
-            }
+            optionsBuilder.UseSqlServer(Util.ConnectionString);
+            base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
