57e6bf8921b71c7d7846bb73e35d1408f1bef9b7
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-11-14 18:20:04 -0600

    Clases de Authentication en BackEnd

---
+++ Northwind/BackEnd/Authentication/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+
+        public List<string> ? Roles { get; set; }
+    }
+}
