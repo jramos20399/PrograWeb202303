285008e7c5371c336c9bf1a941fb43ba4cce2144
juanpabloramosp <juanpabloramosp@hotmail.com>
2023-11-14 18:41:06 -0600

    Archivos de conf

--- Northwind/BackEnd/Program.cs
+++ Northwind/BackEnd/Program.cs
@@ -5,15 +5,38 @@ using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
 using Entities.Utilities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+#region ConnString
+
+string connString = builder
+                            .Configuration
+                            .GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<NorthWindContext>(options =>
+                        options.UseSqlServer(
+                           connString
+                            ));
+
+Util.ConnectionString = connString;
+
+#endregion
+
+
 #region Serilog
 
-    builder.Logging.ClearProviders();
+builder.Logging.ClearProviders();
     builder.Logging.AddConsole();
     builder.Host.UseSerilog((ctx, lc) =>lc
             .WriteTo.File("logs/logsBackEnd.txt", rollingInterval: RollingInterval.Day)
@@ -35,21 +58,66 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+#region Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>( options=>
+
+                {
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                }
+    
+    
+    
+    )
+    .AddEntityFrameworkStores<NorthWindContext>()
+    .AddDefaultTokenProviders();
+
+
+
+#endregion
+
+
+
+
+#region JWT
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
+
+
+
+
+#endregion
+
+
+
+
 #region Dependency Injection
-
-string connString = builder
-                            .Configuration
-                            .GetConnectionString("DefaultConnection");
-
-
-builder.Services.AddDbContext<NorthWindContext>(options =>
-                        options.UseSqlServer(
-                           connString
-                            ));
-
-Util.ConnectionString = connString;
-
-
 builder.Services.AddScoped<ICategoryDAL, CategoryDALImpl>();
 builder.Services.AddScoped<IUnidadDeTrabajo, UnidadDeTrabajo>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
@@ -75,6 +143,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseMiddleware<ApiKeyMiddleware>();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
